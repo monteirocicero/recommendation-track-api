9be0c8258a101a6b6eaf630fc47277b2242b3afe
Cicero Monteiro <cicerolmonteiro@gmail.com>
2021-02-07 10:32:00 -0300

    Initial structure

---
+++ src/main/java/com/orecic/recommentationtrackapi/RecommentationTrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.orecic.recommentationtrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommentationTrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommentationTrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orecic/recommentationtrackapi/RecommentationTrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orecic.recommentationtrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommentationTrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
